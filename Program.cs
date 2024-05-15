3c4cddf062afb63d45b77f17a2c2cbe9b1d9676e
Zandar Beker (1000126240) <BEKEZA1@student.op.ac.nz>
2024-05-16 10:14:36 +1200

    Added program

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace MultiMethodProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            //LAB1
+
+
+
+
+
+
+
+
+
+
+
+            //LAB2
+
+
+
+
+
+
+
+
+
+
+            //LAB3
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
